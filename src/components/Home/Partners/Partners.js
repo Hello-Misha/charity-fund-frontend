ebdfbabcdabaa7610049c1713bb4751256445d8e
Misha <Matviichuk.Mykhailo@gmail.com>
2022-12-14 20:01:50 +0200

    Localisation fixed

--- src/components/Home/Partners/Partners.js
+++ src/components/Home/Partners/Partners.js
@@ -4,9 +4,9 @@ import { useTranslation } from "react-i18next";
 
 import classes from "./Partners.module.scss";
 
-import SmileTrainX1 from "../../../img/SmileTrain-x1.png";
+import SmileTrainX1 from "../../../img/SmileTrain-X1.png";
 import SmileTrainX2 from "../../../img/SmileTrain-x2.png";
-import BeyondWordsX1 from "../../../img/BeyondWords-x1.png";
+import BeyondWordsX1 from "../../../img/BeyondWords-X1.png";
 import BeyondWordsX2 from "../../../img/BeyondWords-x2.png";
 
 function Partners() {

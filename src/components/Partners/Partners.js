5ceb72f04cd7cd389e195aba3631ccb119cae3a4
Misha <Matviichuk.Mykhailo@gmail.com>
2022-12-14 17:31:56 +0200

    PartnersImg fix

---
+++ src/components/Partners/Partners.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+import { useTranslation } from "react-i18next";
+
+import classes from "./Partners.module.scss";
+
+import SmileTrainX1 from "../../img/SmileTrain-X1.png";
+import SmileTrainX2 from "../../img/SmileTrain-x2.png";
+import BeyondWordsX1 from "../../img/BeyondWords-x1.png";
+import BeyondWordsX2 from "../../img/BeyondWords-x2.png";
+
+function Partners() {
+  const { t } = useTranslation();
+  return (
+    <section className={classes.section}>
+      <div className={`${classes.section__partner}`}>
+        <img
+          src={SmileTrainX1}
+          srcset={`${SmileTrainX1} 300w, ${SmileTrainX2} 768w`}
+          alt="SmileTrain logo"
+          className={`${classes.section__partner__img}`}
+        />
+
+        <p className={`Text White ${classes.section__partner__text} right`}>
+          {t("homePage.partners.partner_1")}
+        </p>
+        <span
+          className={`${classes.section__circle} ${classes.section__CirclePosition_1}`}
+        ></span>
+      </div>
+
+      <div className={`${classes.section__partner}`}>
+        <p className={`Text White ${classes.section__partner__text} left`}>
+          {t("homePage.partners.partner_2")}
+        </p>
+
+        <img
+          src={BeyondWordsX1}
+          srcset={`${BeyondWordsX1} 300w, ${BeyondWordsX2} 768w`}
+          alt="BeyondWords logo"
+          className={`${classes.section__partner__img}`}
+        />
+        <span
+          className={`${classes.section__circle} ${classes.section__CirclePosition_2}`}
+        ></span>
+      </div>
+    </section>
+  );
+}
+
+export default Partners;
